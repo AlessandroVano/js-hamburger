d6500e32bc8041f2a6c488b1e575092ab5f8ea71
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-23 13:57:15 +0200

    seleziono la costante di hamburger menù

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 // Mostrare / nascondere menù principale
 const hamburger = document.querySelector('.header-right > a');
 console.log('header-right');
+
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+console.log('Hamburger-menu');
