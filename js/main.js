96647d57bcd1479754c02f76ace9b038849e0efd
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-23 13:58:23 +0200

    Apertura menu hamburgher

--- js/main.js
+++ js/main.js
@@ -4,3 +4,11 @@ console.log('header-right');
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 console.log('Hamburger-menu');
+
+
+//Apertura menù hamburger
+
+hamburger.addEventListener('click', function(){
+    hamburgerMenu.classList.add('active');
+    console.log('active');
+});
