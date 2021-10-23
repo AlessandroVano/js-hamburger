2cacc2299fdce5baf1bc5e48664ddb31094e41c4
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-10-23 14:00:03 +0200

    chiusura menu hamburger ( non funziona)

--- js/main.js
+++ js/main.js
@@ -5,10 +5,16 @@ console.log('header-right');
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 console.log('Hamburger-menu');
 
+const close = document.querySelector('close');
+console.log('close');
 
 //Apertura menù hamburger
-
 hamburger.addEventListener('click', function(){
     hamburgerMenu.classList.add('active');
     console.log('active');
+});
+
+// Chiusura menù hamburger
+close.addEventListener('click', function(){
+    hamburgerMenu.classList.remove('active');
 });
